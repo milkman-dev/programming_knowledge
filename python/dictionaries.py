fca65631a05948f0836b1440f3d1426aff865f1c
Logan Milkman <loganescobarc@gmail.com>
2023-07-09 14:03:48 -0600

    I forgot to save locally and commit

---
+++ python/dictionaries.py
@@ -0,0 +1,77 @@
+''' Dictionaries are like lists but for each item
+there is a key and a value, keys can only be unhashable
+and values can be any data type'''
+
+# Declaring an empty dictionary
+
+new_dictionary = {}
+
+# Dictionary with item (String) and value (String)
+
+new_dictionary = {"item": "value"}
+
+# Dictionary with different data type as a value
+
+new_dictionary = {"item": {"new_item": "new_value"}}
+new_dictionary = {"item": [1, 2, 3, 4, 5]}
+
+# Add a single item to a dictionary
+
+new_dictionary["new_item"] = "new_value"
+
+# Add multiple items to a dictionary
+
+new_dictionary.update({"item1": "value1", "item2": "value2", "item3": "value3"})
+
+# Overwrite an item value
+
+new_dictionary["item"] = "new_value"
+
+inventory = {"iron spear": 12, "invisible knife": 30, "needle of ambition": 10, "stone glove": 20}
+ 
+inventory["invisible knife"] = 40
+inventory["mithril shield"] = 25
+
+# Just as list comprehensions there are dictionary comprehensions (Cool!)
+
+drinks = ["espresso", "chai", "decaf", "drip"]
+caffeine = [64, 40, 0, 120]
+
+zipped_drinks = zip(drinks, caffeine)
+
+drinks_to_caffeine = {item: value for item, value in zipped_drinks}
+drinks_to_caffeine = {drinks[item]: caffeine[item] for item in range(len(drinks))}
+drinks_to_caffeine = {item: value for item, value in zip(drinks, caffeine)}
+
+# Practicing how Spotify may work with dict:
+
+# ---
+
+# 2 Lists
+
+songs = ["Like a Rolling Stone", "Satisfaction", "Imagine", "What's Going On", "Respect", "Good Vibrations"]
+playcounts = [78, 29, 44, 21, 89, 5]
+
+# Make a dict with dict comprehension
+
+plays = {item: value for item, value in zip(songs, playcounts)}
+
+# o.o
+
+print(plays)
+
+# Add new song
+
+plays["Purple Haze"] = 1
+
+# I listened to more songs from Aretha Franklin
+
+plays["Respect"] = 94
+
+# New dict
+
+library = {"The Best Songs": plays, "Sunday Feelings": {}}
+
+print(library)
+
+# ---
