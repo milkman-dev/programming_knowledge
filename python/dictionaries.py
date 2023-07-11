1e35a20f75cdca3ae302dcb5e03b1035822dd8d6
Logan Milkman <loganescobarc@gmail.com>
2023-07-10 22:27:32 -0600

    Added some practice examples

--- python/dictionaries.py
+++ python/dictionaries.py
@@ -126,3 +126,34 @@ print(users)
 print(lessons)
 
 # .values() method works to give the values of a dictionary
+
+num_exercises = {"functions": 10, "syntax": 13, "control flow": 15, "loops": 22, "lists": 19, "classes": 18, "dictionaries": 18}
+
+total_exercises = 0
+
+for grade in num_exercises.values():
+  total_exercises += grade
+
+print(total_exercises)
+
+# Get both keys and values with the .items() method!
+
+pct_women_in_occupation = {"CEO": 28, "Engineering Manager": 9, "Pharmacist": 58, "Physician": 40, "Lawyer": 37, "Aerospace Engineer": 9}
+
+for occupation, age in pct_women_in_occupation.items():
+  print("Women make up {} percent of {}".format(age, occupation))
+
+# Practice
+
+tarot = { 1:	"The Magician", 2:	"The High Priestess", 3:	"The Empress", 4:	"The Emperor", 5:	"The Hierophant", 6:	"The Lovers", 7:	"The Chariot", 8:	"Strength", 9:	"The Hermit", 10:	"Wheel of Fortune", 11:	"Justice", 12:	"The Hanged Man", 13:	"Death", 14:	"Temperance", 15:	"The Devil", 16:	"The Tower", 17:	"The Star", 18:	"The Moon", 19:	"The Sun", 20:	"Judgement", 21:	"The World", 22: "The Fool"}
+
+spread = {}
+
+spread["past"] = tarot.pop(13)
+
+spread["present"] = tarot.pop(22)
+
+spread["future"] = tarot.pop(10)
+
+for key, value in spread.items():
+  print("Your {} is the {} card.".format(key, value))
