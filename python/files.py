be9c6bc0629e1c1ed3924a7c8f5e925d263aa847
Logan Milkman <loganescobarc@gmail.com>
2023-07-18 17:32:37 -0600

    Added JSON exercises with file opening

--- python/files.py
+++ python/files.py
@@ -89,3 +89,13 @@ with open("message.json") as message_json:
   message = json.load(message_json)
 
 print(message['text'])
+
+# To write a json file we can do the following
+
+data_payload = [
+  {'interesting message': 'What is JSON? A web application\'s little pile of secrets.',
+   'follow up': 'But enough talk!'}
+]
+
+with open('data.json', 'w') as data_json:
+   json.dump(data_payload, data_json)
