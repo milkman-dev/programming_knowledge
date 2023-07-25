83a2f9ce3163ca7b2a55801b4e22b6bf4986f06f
Logan Milkman <loganescobarc@gmail.com>
2023-07-25 17:44:08 -0600

    Added classes directory

---
+++ python/classes/classes.py
@@ -0,0 +1,11 @@
+''' There are different programming languages, python is an OOP (Objected oriented programming) 
+language this means that we can make "objects" which are basically data types that we create, this
+are called "CLASSES"'''
+
+# To define a class we begin with the class keyword
+
+class My_class:
+    pass # pass keyword is used so that the terminal doesn't print an error
+
+''' Classes can have methods (functions inside the class) and also attributes (variables) inside
+'''
