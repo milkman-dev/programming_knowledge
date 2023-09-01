1bffba2fd228afae225dec1677cf6d1471786567
Logan Milkman <loganescobarc@gmail.com>
2023-08-31 22:41:12 -0600

    Created a new directory

--- go/functions.go
+++ go/functions.go
@@ -1,24 +1,79 @@
+// DB functions
+
 func queryDatabase(query string) string {
 	var result string
 	connectDatabase()
 	// Add deferred call to disconnectDatabase() here
 	defer disconnectDatabase()
-  
+
 	if query == "SELECT * FROM coolTable;" {
-	  result = "NAME|DOB\nVincent Van Gogh|March 30, 1853"
-	}  
+		result = "NAME|DOB\nVincent Van Gogh|March 30, 1853"
+	}
 	fmt.Println(result)
 	return result
-  }
-  
-  func connectDatabase() {
+}
+
+func connectDatabase() {
 	fmt.Println("Connecting to the database.")
-  }
-  
-  func disconnectDatabase() {
+}
+
+func disconnectDatabase() {
 	fmt.Println("Disconnecting from the database.")
-  }
-  
-  func main() {
+}
+
+func main() {
 	queryDatabase("SELECT * FROM coolTable;")
-  }
+}
+
+// Spaceship functions
+
+func fuelGauge(fuel int) {
+	fmt.Println("You have", fuel, "gallons of fuel left")
+}
+
+func calculateFuel(planet string) int {
+	var fuel int
+	switch planet {
+	case "Venus":
+		fuel = 300000
+	case "Mercury":
+		fuel = 500000
+	case "Mars":
+		fuel = 700000
+	default:
+		fuel = 0
+	}
+	return fuel
+}
+
+func greetPlanet(planet string) {
+	fmt.Println("You have arrived to", planet)
+}
+
+func cantFly() {
+	fmt.Println("We do not have the available fuel to fly there.")
+}
+
+func flyToPlanet(planet string, fuel int) int {
+	var fuelRemaining, fuelCost int
+	fuelRemaining = fuel
+	fuelCost = calculateFuel(planet)
+	if fuelRemaining >= fuelCost {
+		greetPlanet(planet)
+		fuelRemaining = fuelRemaining - fuelCost
+	} else {
+		cantFly()
+	}
+	return fuelRemaining
+}
+
+func main() {
+	var fuel int
+	var planetChoice string
+	fuel = 1000000
+	planetChoice = "Venus"
+
+	fuel = flyToPlanet(planetChoice, fuel)
+	fuelGauge(fuel)
+
+}
